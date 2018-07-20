72dc6fb8b9f891b802c6013639f6f0abb353abbb
zrphercule <zrphercule@gmail.com>
2018-07-20 16:22:32 -0700

    minmax_ops

--- caffe2/operators/minmax_ops.h
+++ caffe2/operators/minmax_ops.h
@@ -6,6 +6,8 @@
 #include "caffe2/core/logging.h"
 #include "caffe2/core/operator.h"
 #include "caffe2/core/types.h"
+#include "caffe2/operators/elementwise_ops.h"
+#include "caffe2/operators/elementwise_ops_utils.h"
 #include "caffe2/utils/math.h"
 
 namespace caffe2 {
@@ -17,29 +19,32 @@ class MaxMinOpBase : public Operator<Context> {
   USE_SIMPLE_CTOR_DTOR(MaxMinOpBase)
 
   bool RunOnDevice() override {
+	std::vector<int> input0_dims(Input(0).dims().begin(), Input(0).dims().end());
+	std::vector<int> output_dims(input0_dims);
     auto& input0 = Input(0);
-    auto* output = Output(0);
+	auto* output = Output(0);
 
-    output->ResizeLike(input0);
-    output->CopyFrom(input0, &context_);
 
+	for (int i = 1; i < InputSize(); i++){
+		std::vector<int> inputi_dims(Input(i).dims().begin(), Input(i).dims().end());
+		output_dims = elementwise_ops_utils::ComputeBinaryBroadcastForwardDims(
+				output_dims,
+				inputi_dims);
+	}
+
+    output->Resize(output_dims);
+	math::Broadcast<caffe2::Tensor<Context>, Context>(
+		input0_dims.size(),
+		input0_dims.data(),
+		output_dims.size(),
+		output_dims.data(),
+		&input0,
+		output,
+		&context_);
+	printf("InputSize is %d\n", InputSize());
     if (InputSize() == 1) {
       return true;
     }
-
-    // Dimension checking
-    for (int i = 1; i < InputSize(); ++i) {
-      CAFFE_ENFORCE_EQ(
-          output->dims(),
-          Input(i).dims(),
-          "Description: Input #",
-          i,
-          ", input dimension:",
-          Input(i).dims(),
-          " should match output dimension: ",
-          output->dims());
-    }
-
     return this->Compute();
   }
 
